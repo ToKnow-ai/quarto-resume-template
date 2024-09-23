2adc20242a42fa65737c088ae174471159eeb0a1
Kabue Charles <mckabue@gmail.com>
2024-09-24 02:27:40 +0300

    Refactor flex-minipage-filter.lua to use RawInline for LaTeX commands, improving performance and readability

--- flex-minipage-filter.lua
+++ flex-minipage-filter.lua
@@ -6,22 +6,13 @@ local function flex_minipage_filter(doc)
         ---@return pandoc.Block
         Block = function(block)
             if block.attr and block.attr.classes and block.attr.classes:includes("d-flex") then
-                block.content:insert(
-                    1,
-                    pandoc.Div(pandoc.List {
-                        pandoc.RawBlock('latex', "\\borderBox {"),
-                        pandoc.RawBlock('latex', "\\begingroup"),
-                        pandoc.RawBlock('latex', "\\let\\par\\relax"),
-                        pandoc.RawBlock('latex', "\\begin{minipage}[t]{0.6\\textwidth}")
-                    })
-            )
+                block.content:insert(1, pandoc.RawInline('latex', "\\borderBox {"))
+                block.content:insert(2, pandoc.RawInline('latex', "\\begingroup"))
+                block.content:insert(3, pandoc.RawInline('latex', "\\let\\par\\relax"))
+                block.content:insert(4, pandoc.RawInline('latex', "\\begin{minipage}[t]{0.6\\textwidth}"))
                 -- The first block
-                block.content:insert(
-                    3, 
-                    pandoc.Div(pandoc.List {
-                        pandoc.RawBlock('latex', "\\end{minipage}"),
-                        pandoc.RawBlock('latex', "\\hfill")
-                    }))
+                block.content:insert(6, pandoc.RawInline('latex', "\\end{minipage}"))
+                block.content:insert(7, pandoc.RawInline('latex', "\\hfill"))
                 block.content:insert(pandoc.RawInline('latex', "\\endgroup"))
                 block.content:insert(pandoc.RawInline('latex', "}"))
 
